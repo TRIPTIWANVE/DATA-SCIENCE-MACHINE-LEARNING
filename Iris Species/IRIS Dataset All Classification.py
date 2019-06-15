2ef42c4f816f14aae193ebd2a06a02bcaefb89d3
TRIPTIWANVE <51852631+TRIPTIWANVE@users.noreply.github.com>
2019-06-15 19:42:49 +0530

    Add files via upload

---
+++ Iris Species/IRIS Dataset All Classification.py
@@ -0,0 +1,127 @@
+# Iris data set KNN
+
+import pandas as pd
+iris_data=pd.read_excel('../DATASET/Irisdata.xls')
+
+colnames=iris_data.columns.values
+print(colnames)
+x=iris_data[colnames[0:4]]
+y=iris_data[colnames[-1]]
+print(x)
+print(y)
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_val,y_train,y_val = train_test_split(x,y,train_size=0.7,random_state=0)
+print(x_train.shape)
+print(x_val.shape)
+print(y_train.shape)
+print(y_val.shape)
+
+from sklearn.metrics import accuracy_score
+
+print("-"*100)
+
+# Gaussian Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+gaussian = GaussianNB()
+gaussian.fit(x_train, y_train)
+y_pred = gaussian.predict(x_val)
+acc_gaussian = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("Naive Bayes",acc_gaussian)
+print("-"*100)
+
+
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+logreg.fit(x_train, y_train)
+y_pred = logreg.predict(x_val)
+acc_logreg = round(accuracy_score(y_pred, y_val)*100,2)
+print("Logistic Regression",acc_logreg)
+print("-"*100)
+
+# Support Vector Machines
+from sklearn.svm import SVC
+
+svc = SVC()
+svc.fit(x_train, y_train)
+y_pred = svc.predict(x_val)
+acc_svc = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("SVM",acc_svc)
+print("-"*100)
+
+# Linear SVC
+from sklearn.svm import LinearSVC
+
+linear_svc = LinearSVC()
+linear_svc.fit(x_train, y_train)
+y_pred = linear_svc.predict(x_val)
+acc_linear_svc = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("LINEAR SVC",acc_linear_svc)
+print("-"*100)
+
+# Perceptron
+from sklearn.linear_model import Perceptron
+perceptron = Perceptron()
+perceptron.fit(x_train, y_train)
+y_pred = perceptron.predict(x_val)
+acc_perceptron = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("Perceptron",acc_perceptron)
+print("-"*100)
+
+#Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+decisiontree = DecisionTreeClassifier()
+decisiontree.fit(x_train, y_train)
+y_pred = decisiontree.predict(x_val)
+acc_decisiontree = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("Decision Tree",acc_decisiontree)
+print("-"*100)
+
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
+randomforest = RandomForestClassifier()
+randomforest.fit(x_train, y_train)
+y_pred = randomforest.predict(x_val)
+acc_randomforest = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("RandomForest",acc_randomforest)
+print("-"*100)
+
+# KNN or k-Nearest Neighbors
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_val)
+acc_knn = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("K-NN",acc_knn)
+print("-"*100)
+
+# Stochastic Gradient Descent
+from sklearn.linear_model import SGDClassifier
+sgd = SGDClassifier()
+sgd.fit(x_train, y_train)
+y_pred = sgd.predict(x_val)
+acc_sgd = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("SGD",acc_sgd)
+print("-"*100)
+
+# Gradient Boosting Classifier
+from sklearn.ensemble import GradientBoostingClassifier
+gbk = GradientBoostingClassifier()
+gbk.fit(x_train, y_train)
+y_pred = gbk.predict(x_val)
+acc_gbk = round(accuracy_score(y_pred, y_val) * 100, 2)
+print("GradientBoostingClassifier",acc_gbk)
+print("-"*100)
+
+#all in a dictionary
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression',
+              'Random Forest', 'Naive Bayes', 'Perceptron', 'Linear SVC',
+              'Decision Tree', 'Stochastic Gradient Descent', 'Gradient Boosting Classifier'],
+    'Score': [acc_svc, acc_knn, acc_logreg,
+              acc_randomforest, acc_gaussian, acc_perceptron,acc_linear_svc, acc_decisiontree,
+              acc_sgd, acc_gbk]})
+print(models.sort_values(by='Score', ascending=False))
